6e6d438df439ce48f5fbe8ab404111b3b488f107
Nick Makes <nick.makes@mlb.com>
2016-01-22 13:28:14 -0500

    Adds scroll spy as angular directives (first cut)

--- src/fairfield-league-constitution.js
+++ src/fairfield-league-constitution.js
@@ -19,6 +19,59 @@
 		} );
 	} )
 
+	.directive( 'scrollSpy', function($timeout) {
+		return {
+			restrict : 'A',
+			link : function(scope, elem, attr) {
+				var offset = parseInt( attr.scrollOffset, 10 );
+				if ( !offset )
+					offset = 10;
+				elem.scrollspy( {
+					'offset' : offset
+				} );
+				scope.$watch( attr.scrollSpy, function(value) {
+					$timeout( function() {
+						elem.scrollspy( 'refresh', {
+							'offset' : offset
+						} )
+					}, 1 );
+				}, true );
+			}
+		}
+	} )
+
+	.directive( 'preventDefault', function() {
+		return {
+			restrict : 'A',
+			link : function(scope, elem, attr) {
+				jQuery( element ).click( function(event) {
+					event.preventDefault();
+				} );
+			}
+		}
+	} )
+
+	.directive( 'scrollTo', [ '$window', function($window) {
+		return {
+			restrict : 'AC',
+			compile : function() {
+				function scrollInto(elementId) {
+					if ( !elementId )
+						$window.scrollTo( 0, 0 );
+					var element = document.getElementById( elementId );
+					if ( elem )
+						element.scrollIntoView();
+
+					return function(scope, elem, attr) {
+						elem.bind( 'click', function(event) {
+							scrollInto( attr.scrollTo );
+						} )
+					}
+				}
+			}
+		}
+	} ] )
+
 	.controller( 'FlcCtrl', [ FlcCtrl ] );
 
 	function FlcCtrl() {
