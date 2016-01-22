7a650a7fca40029c335594a4d5377bb281eef612
Nick Makes <nick.makes@mlb.com>
2016-01-21 22:31:31 -0500

    Initializes constitution directive

---
+++ src/fairfield-league-constitution.js
@@ -0,0 +1,29 @@
+(function() {
+	'use strict';
+
+	function FlcCtrl() {
+		var vm = this;
+	}
+
+	angular.module( 'app.fairfieldLeagueConstitution', [ 'ui.router' ] )
+
+	.config( function config($stateProvider) {
+		$stateProvider.state( 'tito', {
+			url : '/fairfieldLeagueConstitution',
+			templateUrl : 'constitution.tmpl.html',
+			controller : 'FlcCtrl',
+			controllerAs : 'flc',
+			data : {
+				pageTitle : 'fairfield league constitution'
+			}
+		} );
+	} )
+
+	.controller( 'FlcCtrl', [ FlcCtrl ] );
+
+	function FlcCtrl() {
+		var vm = this;
+	}
+
+	 @@templateCache
+}());
