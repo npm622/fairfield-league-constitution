7d2bcd1e4f4e5e7656ba5295fcce78ec9885e222
Nick Makes <nick.makes@mlb.com>
2016-01-21 23:18:16 -0500

    Adds css styling and new html text

--- src/fairfield-league-constitution.js
+++ src/fairfield-league-constitution.js
@@ -25,5 +25,19 @@
 		var vm = this;
 	}
 
+	// $('#sidebar').affix({
+	// offset: {
+	// top: 245
+	// }
+	// });
+
+	// var $body = $(document.body);
+	// var navHeight = $('.navbar').outerHeight(true) + 10;
+
+	// $body.scrollspy({
+	// target: '#leftCol',
+	// offset: navHeight
+	// });
+
 	 @@templateCache
 }());
