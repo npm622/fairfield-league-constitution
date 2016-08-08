ad5efbbe7dbe557bc15be8e17eb4893f2a53b1d5
Nick Makes <nick.makes@mlb.com>
2016-08-08 14:57:45 -0400

    Adds default "" values to scroller-spy and prevent-default directives

--- src/directives/scroller-spy/scroller-spy.js
+++ src/directives/scroller-spy/scroller-spy.js
@@ -11,9 +11,11 @@
                 if ( !offset )
                     offset = 10;
                 console.log( "offset:  " + offset );
+                
                 $( elem ).scrollspy( {
                     "offset" : offset
                 } );
+                
                 scope.$watch( attr.scrollSpy, function( value ) {
                     $timeout( function() {
                         $( elem ).scrollspy( 'refresh', {
